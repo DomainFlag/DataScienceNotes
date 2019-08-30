bugs fixed, better precision and fault tolerance for in-game data generation

modules/reinforcement-learning-racing/modules/model.py
@@ -5,8 +5,10 @@ import torch.nn as nn
 import torch.nn.functional as fc
 
 from torch.optim.rmsprop import RMSprop
-from torch.tensor import Tensor
-from typing import List, Callable, Optional, Any, Union, NewType
+from collections import namedtuple
+from typing import List, Callable, Optional, Any
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
 
 class DQN(nn.Module):
@@ -56,26 +58,6 @@ class DQN(nn.Module):
         return self.ln(inputs.view(inputs.size(0), -1))
 
 
-class Transition:
-
-    loopable = NewType('loopable', Union[None, list, np.ndarray, Tensor])
-
-    state: Optional[Tensor] = None
-    action: Tensor
-    state_result: Optional[Tensor] = None
-    reward: Tensor
-
-    def __init__(self, state: loopable, action: Tensor, state_result: loopable, reward: Tensor):
-        if state is not None:
-            self.state = state if isinstance(state, Tensor) else torch.FloatTensor(state)
-
-        self.action = action
-
-        if state_result is not None:
-            self.state_result = state_result if isinstance(state_result, Tensor) else torch.FloatTensor(state_result)
-        self.reward = reward
-
-
 class ReplayMemory:
 
     MEMORY_CAPACITY: int = 30000
@@ -99,9 +81,17 @@ class ReplayMemory:
 
         self.position = (self.position + 1) % self.capacity
 
-    def sample(self, batch_size, bagging = False) -> Optional[List[Transition]]:
+    def sample_batch_transition(self, batch_size, bagging = False) -> Optional[List[Transition]]:
         if not bagging or np.random.random() < ReplayMemory.bagging:
             return random.sample(self.memory, batch_size)
+
+        return None
+
+    def sample_transition_batch(self, batch_size, bagging = False) -> Optional[Transition]:
+        transitions = self.sample_batch_transition(batch_size, bagging = bagging)
+
+        if transitions is not None:
+            return Transition(*zip(*transitions))
 
         return None
 
@@ -111,7 +101,7 @@ class ReplayMemory:
 
 class Agent:
 
-    BATCH_SIZE = 32
+    BATCH_SIZE = 4
     GAMMA = 0.999
     EPS_START = 0.9
     EPS_END = 0.05
@@ -174,8 +164,17 @@ class Agent:
         if len(self.memory) < Agent.BATCH_SIZE:
             return
 
-        # TODO(0) - Rework and optimization needed
-        transitions = self.memory.sample(Agent.BATCH_SIZE)
+        # Memory transitions
+        transition_batch = self.memory.sample_transition_batch(Agent.BATCH_SIZE)
+
+        print(transition_batch.reward)
+
+        state_batch = torch.cat(transition_batch.state)
+        action_batch = torch.cat(transition_batch.action)
+        reward_batch = torch.cat(transition_batch.reward)
+
+        print(reward_batch)
+        return
 
         states_prev = torch.FloatTensor([ t.state.numpy() for t in transitions if t.state is not None ])
         states_curr = torch.FloatTensor([ t.state_result.numpy() for t in transitions if t.state_result is not None ])

modules/reinforcement-learning-racing/modules/track.py
@@ -19,6 +19,7 @@ ROAD_HINT = (143, 152, 86)
 class Track:
 
     TRACK_PRECISION: int = 5000
+    TRACK_OFFSET: int = TRACK_PRECISION // 10
     WIDTH_MAX: int = 60
 
     sprite: Sprite = None
@@ -26,6 +27,7 @@ class Track:
     lap: int = 0
     width: float = 0.
     index: int = 0
+    progress: int = 0
 
     static_params: dict = {
         "width_max": WIDTH_MAX,
@@ -150,14 +152,27 @@ class Track:
     def act(self, scaling):
         self.sprite.act(scaling)
 
-        curr_index = self.get_sprite_index(self.sprite.position, self.index)
-        if curr_index < self.index:
+        index = self.get_sprite_index(self.sprite.position, self.index)
+        if abs(self.index - index) > Track.TRACK_OFFSET:
+            # The sprite is located between the start/end extremity
+            index_lower, index_upper = (index, self.index) if (self.index > index) else (self.index, index)
+
+            offset = index_lower - (index_upper - len(self.track_data))
+        else:
+            offset = index - self.index
+
+        self.pts, self.width, self.rot = self.get_sprite_boundaries(self.sprite.position, self.index)
+        self.index = index
+        self.progress += offset
+
+        if self.progress >= len(self.track_data):
+            self.progress %= len(self.track_data)
             self.lap += 1
+        elif self.progress < 0:
+            self.progress = len(self.track_data) + self.progress
+            self.lap -= 1
 
             return True
-
-        self.index = curr_index
-        self.pts, self.width, self.rot = self.get_sprite_boundaries(self.sprite.position, self.index)
 
         return False
 
@@ -179,6 +194,8 @@ class Track:
         params.update({
             "width": self.width,
             "index": self.index,
+            "lap": self.lap,
+            "progress": (self.progress, self.progress / self.TRACK_PRECISION * 100),
             "alive": self.is_alive()
         })
 
@@ -267,28 +284,29 @@ def pivot_map_generate(size, width, height, threshold_dist = 0.75, threshold_ang
     return points, pivots
 
 
-def pivot_adjacent(pivots, index):
-    if index == 0:
-        return np.concatenate(([pivots[-1]], pivots[index:index + 2])), [len(pivots) - 1, index, index + 1]
-    elif index == len(pivots) - 1:
-        return np.concatenate((pivots[index - 1:index + 1], [pivots[0]])), [index - 1, index, 0]
-    else:
-        return np.array(pivots[index - 1:index + 2]), list(range(index - 1, index + 2))
+def pivot_adjacent(pivots, index, boundary = True):
+    indices = [ x % len(pivots) for x in range(index, index + 3) ]
+    points = [ pivots[x % len(pivots)] for x in indices if not boundary or not x == 0 ]
+
+    if boundary and 0 in indices:
+        points.append(pivots[(indices[-1] + 1) % len(pivots)])
+
+    return np.array(points), indices
 
 
-def shape_coordinates(pivots, scaling = Track.WIDTH_MAX / 2.0):
-    if len(pivots) != 3:
-        raise Exception("Requires three pivots")
+def shape_coordinates(points, scaling = Track.WIDTH_MAX / 2.0):
+    if len(points) != 3:
+        raise Exception("Requires three points")
 
-    ab, bc, ac = calc_distance(*pivots[0:2]), calc_distance(*pivots[1:]), calc_distance(pivots[0], pivots[2])
-    width, height = (pivots[2] - pivots[1])
+    ab, bc, ac = calc_distance(*points[0:2]), calc_distance(*points[1:]), calc_distance(points[0], points[2])
+    width, height = (points[2] - points[1])
 
     ac1 = (bc ** 2 + ac ** 2 - ab ** 2) / (2 * ac)
     orientation, alpha = np.arctan2(height, width), np.arcsin(ac1 / bc)
     angle1, angle2 = orientation - alpha, orientation + (np.pi - alpha)
 
-    x0 = np.array([np.cos(angle1), np.sin(angle1)]) * scaling + pivots[1]
-    x1 = np.array([np.cos(angle2), np.sin(angle2)]) * scaling + pivots[1]
+    x0 = np.array([np.cos(angle1), np.sin(angle1)]) * scaling + points[1]
+    x1 = np.array([np.cos(angle2), np.sin(angle2)]) * scaling + points[1]
 
     return [x0, x1], orientation, bc
 
@@ -303,6 +321,7 @@ def interpolated_map_generate(pivots, noise = None, knots = 3, order = False, pr
     xs, ys = np.r_[xs, xs[0]], np.r_[ys, ys[0]]
     weights = noise if noise is not None else None
 
+    # Create the B-spline representation that fits most the data points
     tck, u = interpolate.splprep([xs, ys], w = weights, s = 0, t = knots, per = True)
 
     # Evaluate the spline fits for precision* evenly spaced distance values
@@ -311,7 +330,7 @@ def interpolated_map_generate(pivots, noise = None, knots = 3, order = False, pr
     return xi, yi
 
 
-def draw_aaline(surface, line, color, width = 1.0, length = None):
+def draw_line_aliased(surface, line, color, width = 1.0, length = None):
     center = np.mean(line, axis = 0)
 
     if length is None:
@@ -343,10 +362,10 @@ def track_line_render(step, offset):
             pos1 = np.array([x1, y1]) + track_offset
             pos2 = np.array([xi[ind], yi[ind]]) + track_offset
 
-            draw_aaline(screen, [pos1, pos2], ROAD_TRACK, width = Track.WIDTH_MAX)
+            draw_line_aliased(screen, [pos1, pos2], ROAD_TRACK, width = Track.WIDTH_MAX)
 
             if index % step > offset:
-                draw_aaline(screen, [pos1, pos2], ROAD_HINT, width = 2, length = 4.0)
+                draw_line_aliased(screen, [pos1, pos2], ROAD_HINT, width = 2, length = 4.0)
 
     return renderer
 

modules/reinforcement-learning-racing/racing_game.py
@@ -4,7 +4,6 @@ import os
 import torch
 
 from PIL import Image
-from typing import Optional
 from modules import Track, Sprite, Agent, Transition
 
 TITLE = "RL racer"
@@ -55,19 +54,25 @@ def create_snapshot(surface, filename: str = None, format = "PNG", save = False,
 
 
 def smoothness(x):
-    return np.sqrt(np.log10(x))
+    """ X value is expected to be normalized - [0, 1] """
+    assert(0.0 <= x <= 1.0)
+
+    return np.sqrt(np.log(x + 1.0) / np.log(2))
 
 
-def rewarder(prev_params: Optional[dict], curr_params: dict):
+def rewarder(prev_params: dict, curr_params: dict):
     if not curr_params["alive"]:
         return -1.0
 
-    reward_acc = smoothness(curr_params["acc"] / curr_params["acc_max"] * (np.e - 1))
-    if prev_params is not None and curr_params["acc"] < prev_params["acc"]:
-        reward_acc = np.sqrt(reward_acc)
+    if curr_params["acc"] >= 0.0:
+        reward_acc = smoothness(curr_params["acc"] / curr_params["acc_max"])
+        if curr_params["acc"] < prev_params["acc"]:
+            reward_acc = np.sqrt(reward_acc)
+    else:
+        reward_acc = -0.25
 
     if curr_params["alive"]:
-        reward_pos = 1.0 - smoothness(curr_params["width"] / curr_params["width_max"] * (np.e - 1))
+        reward_pos = 1.0 - smoothness(curr_params["width"] / curr_params["width_max"])
     else:
         reward_pos = 0
 
@@ -76,12 +81,24 @@ def rewarder(prev_params: Optional[dict], curr_params: dict):
     return reward
 
 
-def racing_game(agent_active = True, episode_count = 25):
+def get_caption_renderer(clock = False):
+    if clock:
+        message = "{:s}: {:.2f}fps, index - {:d}, progress - {:5.2f}%, lap - {:d}"
+    else:
+        message = "{:s}: index - {:d}, progress - {:5.2f}%, lap - {:d}"
+
+    def renderer(args):
+        pygame.display.set_caption(message.format(*args))
+
+    return renderer
+
+
+def racing_game(agent_active = False, episode_count = 25):
 
     # Set full screen centered
     os.environ['SDL_VIDEO_CENTERED'] = '1'
     pygame.init()
-    pygame.display.set_caption(TITLE)
+    caption_renderer = get_caption_renderer(clock = not agent_active)
 
     # Set the height and width of the screen
     screen = pygame.display.set_mode(SIZE)
@@ -111,12 +128,7 @@ def racing_game(agent_active = True, episode_count = 25):
         # Set up the agent
         agent = Agent(np.array(SIZE), np.array([Sprite.MOTION_SPACE_COUNT, Sprite.STEERING_SPACE_COUNT]), rewarder)
 
-    # States
-    prev_state, curr_state = None, None
-    prev_params, curr_params = None, None
-
-    actions = None
-
+    state, params = None, None
     episode_counter = 0
 
     while not done:
@@ -134,11 +146,6 @@ def racing_game(agent_active = True, episode_count = 25):
                 track.sprite.rotation += track.sprite.steering * attenuation
             elif keys[pygame.K_RIGHT]:
                 track.sprite.rotation -= track.sprite.steering * attenuation
-        elif prev_state is not None:
-            # Generate actions
-            actions = agent.choose_actions(prev_state)
-
-            track.sprite.act_actions(actions)
 
         # User did something
         for event in pygame.event.get():
@@ -167,41 +174,50 @@ def racing_game(agent_active = True, episode_count = 25):
         # Update the screen
         pygame.display.flip()
 
+        params_next = track.get_params()
         if not agent_active:
             # Compute rendering time
             curr_time = pygame.time.get_ticks()
             attenuation, prev_time = (curr_time - prev_time) / (1000 / FPS_CAP), curr_time
 
             # Handle constant FPS cap
-            pygame.display.set_caption("{0}: {1:.2f}".format(TITLE, clock.get_fps()))
+            caption_params = [ TITLE, clock.get_fps(), params_next["index"], params_next["progress"][1], params_next["lap"] ]
 
             clock.tick(FPS_CAP)
         else:
-            curr_state, curr_params = create_snapshot(surface, raw = True, tensor = True), track.get_params()
+            state_next = create_snapshot(surface, raw = True, tensor = True)
 
-            # Create a fresh transition
-            transition = Transition(prev_state, actions, curr_state, rewarder(prev_params, curr_params))
+            # Generate actions
+            actions = agent.choose_actions(state)
+            track.sprite.act_actions(actions)
 
-            # Update the memory state
-            agent.memory.push(transition)
+            # Create transition and update memory state
+            if state is not None:
+                transition = Transition(state, actions, state_next, rewarder(params, params_next))
+                agent.memory.push(transition)
 
             # Optimize model
             agent.optimize_model()
 
+            flag = False
             if lap_finished:
                 episode_counter += 1
                 if episode_counter == episode_count:
                     done = True
                 else:
-                    # Initialize the environment and state
-                    track.reset_track()
+                    flag = True
 
-            if not curr_params["alive"]:
+            flag = flag or not params_next["alive"]
+            if flag:
                 # Initialize the environment and state
                 track.reset_track()
 
-            prev_state, curr_state = curr_state, None
-            prev_params, curr_params = curr_params, None
+                state_next, params_next = None, None
+
+            state, params = state_next, params_next
+            caption_params = [ TITLE, params["index"], params["progress"][1], params["lap"] ]
+
+        caption_renderer(caption_params)
 
     # Be IDLE friendly
     pygame.quit()